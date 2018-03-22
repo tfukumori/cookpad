e78c459a988c7539f3c816f3a9bf3927e84dc25c
tfukumori <t_fukumori@mac.com>
2018-03-22 09:48:01 +0900

    generate前

--- train.py
+++ train.py
@@ -117,8 +117,87 @@ def plot_history(history):
 
     print('plot history ...done.')
 
-
 def instanciate_model(X):
+    print('create the model ...')
+
+    # input_shapeに32:32:3が渡される形。縦、横、RGB
+    in_shape = X.shape[1:]
+
+    # モデルの作成
+    model = Sequential()
+
+    # 畳み込み層の作成
+
+    # model.add(Conv2D(32, (3, 3))) 2次元の層の畳み込み
+    #   カーネル数32, カーネルサイズ(3,3)
+    #   padding=same,畳み込みしても画像サイズが変わらないように
+    #   input_shapeは1層目なので必要。https://keras.io/ja/layers/convolutional/#conv2d
+
+    # model.add(Activation('relu')) 活性化層（発火を判定するための層）。前後の層をreluという活性化関数でつなぐ
+
+    # model.add(Dropout(0.25))
+    #   過学習を防ぐためにある（逆から計算する際にどれだけ学習が固定される、
+    #   ネットワークから切り離されるわけではない、層を深くするのが簡単になる）
+
+    model.add(Conv2D(32, (3, 3), padding="same", data_format=data_format, input_shape=in_shape))  
+    model.add(Activation('relu'))
+
+    model.add(Conv2D(32, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Conv2D(32, (3, 3), padding='same'))
+    model.add(Activation('relu'))
+
+    model.add(Conv2D(32, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Conv2D(64, (3, 3), padding='same'))
+    model.add(Activation('relu'))
+
+    model.add(Conv2D(64, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Dropout(0.25))
+
+    # ２次元から１次元ベクトルに変換する
+    model.add(Flatten())
+
+    # ユニット数を減らす 
+    model.add(Dense(200))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(55))                # 出力層のユニット数は2
+
+    # お約束。シグモイド関数を使う（0-1にして、softmax層を挟みたいため）
+    model.add(Activation('sigmoid'))
+
+    # 学習には寄与しないが、人間のためにある。
+    # 例えば、タコとイカを判定した際に、たこ0.6 イカ0.55という結果になった時に信頼できないということを表したい。
+    # 全体の比率を表してくれる。どのような認識でも、シグモイド関数とソフトマックス層を挟む
+    model.add(Activation('softmax'))
+
+    # 最適化器。誤差を元に係数を修正するためのやり方。
+    # いろいろやり方がある。収束が早い方法(Adam)と汎化が良い方法(SGD)
+    # 最初はAdamで収束したらSGD
+    opt = keras.optimizers.Adam(lr=0.0005, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0003) # 最適化器のセット。lrは学習係数
+
+    # 損失関数を指定している（categorical_crossentropy）
+    # 識別問題の場合は、これ（categorical_crossentropy）結果をどうやって1と0にするか
+    # metricsは学習には寄与しない。人間のため
+    model.compile(optimizer=opt,       # コンパイル
+          loss='categorical_crossentropy',
+          metrics=['accuracy'])
+
+    print(model.summary())
+
+    print('create the model ...done.')
+    return model
+
+
+def instanciate_model_org(X):
     print('create the model ...')
 
     # input_shapeに32:32:3が渡される形。縦、横、RGB
@@ -177,18 +256,20 @@ def train_model(model, X, y):
     print('train the model ...')
 
     # 学習
-    epochs = 20 # 1つのデータ当たりの学習回数
+    # 1つのデータ当たりの学習回数
     # ある程度までは多ければいいが、メモリを食う。多すぎると正しい判定にならない（人生相談に言い換えるとわかりやすいかもしれない）
-    batch_size = 32              # 学習係数を更新するために使う教師データ数
+    epochs = 20
+    # 学習係数を更新するために使う教師データ数
+    batch_size = 100
 
     history = model.fit(X, y, 
         epochs=epochs, 
         batch_size=batch_size, 
-        verbose=1,              # 学習に学習の経過を表示するかどうか
-        validation_split=0.2,   # 渡された教師データのうち、何割を使用するか
-        #validation_data=(x_test, y_test), # validation_dataをセットするとvalidation_splitは無視される
-        shuffle=True,           # 学習毎にデータをシャッフルする。学習の汎化性能を上げる（同じ順番で学習すると学習に相当影響するため。ゲームで同じ順番だとあるパターンにしか対応できなくなるのと一緒）
-        ) # 返り値には、学習中のlossやaccなどが格納される（metricsに指定する必要がある）
+        verbose=1,                          # 学習に学習の経過を表示するかどうか
+        validation_split=0.1,               # 渡された教師データのうち、何割を検証に使用するか
+        #validation_data=(x_test, y_test),  # validation_dataをセットするとvalidation_splitは無視される
+        shuffle=True,                       # 学習毎にデータをシャッフルする。学習の汎化性能を上げる（同じ順番で学習すると学習に相当影響するため。ゲームで同じ順番だとあるパターンにしか対応できなくなるのと一緒）
+        )                                   # 返り値には、学習中のlossやaccなどが格納される（metricsに指定する必要がある）
 
 
     # サマリー
