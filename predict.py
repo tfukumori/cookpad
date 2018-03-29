87a05fbd1d8adec938a9dc65c99851a0100d3884
tfukumori <t_fukumori@mac.com>
2018-03-29 20:11:30 +0900

    submit

--- predict.py
+++ predict.py
@@ -20,6 +20,7 @@ from keras.callbacks import ModelCheckpoint, CSVLogger, LearningRateScheduler, R
 from keras.preprocessing.image import ImageDataGenerator
 from sklearn.model_selection import KFold
 from sklearn.model_selection import train_test_split
+from collections import OrderedDict
 
 
 #data_format = "channels_first"
@@ -27,6 +28,7 @@ data_format = "channels_last"
 
 PATH_TO_TEST_IMAGES = os.path.join('..\data', 'processed', 'test_images')
 PATH_TO_TEST_PICKLE_X = os.path.join('..\data', 'processed', 'test_images_edit_X.pickle')
+PATH_TO_TEST_PICKLE_FILENAME = os.path.join('..\data', 'processed', 'test_file_name.pickle')
 PATH_TO_MODEL = '..\models'
 PATH_TO_MODELPARAM = os.path.join('..\models', 'param')
 PATH_TO_SUBMIT_FILE = 'submit.csv'
@@ -34,7 +36,7 @@ PATH_TO_SUBMIT_FILE = 'submit.csv'
 IMAGE_WIDTH=64
 IMAGE_HEIGHT=64
 
-def load_test_data(path_to_test_images, path_to_images_pickle_x):
+def load_test_data(path_to_test_images, path_to_images_pickle_x, path_to_pickle_file_name):
     print('loading test data ...')
     image_list = []
     X = []
@@ -44,11 +46,15 @@ def load_test_data(path_to_test_images, path_to_images_pickle_x):
     if os.path.isfile(path_to_images_pickle_x):
         with open(path_to_images_pickle_x, 'rb') as f:
             X = pickle.load(f)
+        with open(path_to_pickle_file_name, 'rb') as f:
+            file_name = pickle.load(f)
 
     else:
         for f in file:
             try:
                 im = Image.open(os.path.join(PATH_TO_TEST_IMAGES, f)).resize((IMAGE_WIDTH, IMAGE_HEIGHT))
+                # image_list.append(np.array(im).flatten())
+
                 image = np.array(im)
                 if data_format == "channels_first":
                     image = image.transpose(2, 0, 1)   # 配列を変換し、[[Redの配列],[Greenの配列],[Blueの配列]] のような形にする。
@@ -63,6 +69,8 @@ def load_test_data(path_to_test_images, path_to_images_pickle_x):
 
         with open(path_to_images_pickle_x, 'wb') as f:
             pickle.dump(X, f)
+        with open(path_to_pickle_file_name, 'wb') as f:
+            pickle.dump(file_name, f)
 
     print(X.shape)
 
@@ -71,21 +79,39 @@ def load_test_data(path_to_test_images, path_to_images_pickle_x):
 
 def predict(model, X, file_name):
     print('predicting ...')
-    dic = OrderedDict()
-    dic['file_name'] = file_name
-    dic['predict,ion'] = model.predict(
+    result = model.predict(
         X,
-        batch_size=None,
-        vervose=1
+        None,
+        1
     )
-    print('done.')
-    return pd.DataFrame(dic)
 
-X, file_name = load_data(PATH_TO_TEST_IMAGES, PATH_TO_TEST_PICKLE_X)
+    print('predicting classes ...')
+    resultClasses = model.predict_classes(
+        X,
+        None,
+        1
+    )
+
+    print(str(file_name))
+    print(str(result))
+    print(str(resultClasses))
+
+    df = pd.DataFrame()
+    df['file_name'] = file_name
+    # df['prediction'] = result
+    df['prediction'] = resultClasses
+
+    print('done.')
+    return df
+
+X, file_name = load_test_data(PATH_TO_TEST_IMAGES, PATH_TO_TEST_PICKLE_X, PATH_TO_TEST_PICKLE_FILENAME)
 
 # 機械学習器を復元
-model = model_from_json(open(PATH_TO_MODEL +"\model", 'r').read())
-model.load_weights(PATH_TO_MODELPARAM + "\model.hdf5")
+print('load_model.')
+model = load_model(PATH_TO_MODELPARAM +"\model.continue.hdf5")
+# model = load_model(PATH_TO_MODEL +"\model")
+# print('load_weights.')
+# model.load_weights(PATH_TO_MODELPARAM + "\model.hdf5")
 
 ## output the submit file
 submit = predict(model, X, file_name)
